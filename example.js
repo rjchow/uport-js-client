1588f1a6a39ef7da5a4754c7d7dedc699a3c1777
Zach Ferland <zachferland@gmail.com>
2017-12-07 17:39:24 -0500

    feat: add example for using client as mock client

--- example.js
+++ example.js
@@ -2,6 +2,7 @@ const UPortClient = require('./index.js')
 const deploy = require('./deploy.js')
 const EthJS = require('ethjs-query');
 const HttpProvider = require('ethjs-provider-http');
+const { decodeToken } = require('jsontokens')
 
 const rpcUrl = 'http://127.0.0.1:7545'
 const eth = new EthJS(new HttpProvider(rpcUrl))
@@ -40,3 +41,20 @@ createuPortClient().then(client => {
 }).then(res => {
   // Handle Response
 }).catch(console.log)
+
+// By passing in no newtork configurations you can use the client as a mock client,
+// where it will make no network resquests for TXs, ipfs, or responses, but rather
+// mocks that functionality for testing.
+const creatuPortMockClient = () => {
+  uportClient = new UPortClient()
+  uportClient.initKeys()
+  return uportClient
+}
+
+const mockClient = creatuPortMockClient()
+// const uri = 'me.uport:0x829BD824B016326A401d083B33D092293333A830?value=100'
+const uri = 'me.uport:me?requestToken=eyJ0eXAiOiJKV1QiLCJhbGciOiJFUzI1NksifQ.eyJyZXF1ZXN0ZWQiOlsibmFtZSIsInBob25lIl0sInR5cGUiOiJzaGFyZVJlcSIsImlzcyI6IjB4NWIwYWJiZDM3YmNlYmI5OGEzOTA0NDViNTQwMTE1ZjNjODE5YTNiOSIsImlhdCI6MTQ4NTMyMTEzMzk5Nn0.ZvPhqYLJFa3wdETUcmWGk7Gm4MBNZdfe0eksqRcefwCYaMC96JzWUN0Ot42Pn1SX9M5CMQpkLksC5MQC2mYwgg'
+
+mockClient.consume(uri).then(res => {
+  console.log(decodeToken(res).payload)
+})
