59eea77552c80e8f32bd3314477e769fad2f5532
Zach Ferland <zachferland@gmail.com>
2017-12-07 14:59:36 -0500

    fix: cleanup example

--- example.js
+++ example.js
@@ -1,30 +1,29 @@
-const UPortClient = require('./index.js').UPortMockClient
+const UPortClient = require('./index.js')
 const deploy = require('./deploy.js')
 const EthJS = require('ethjs-query');
 const HttpProvider = require('ethjs-provider-http');
 
-const rpcUrl = 'http://127.0.1.0:7545'
+const rpcUrl = 'http://127.0.0.1:7545'
 const eth = new EthJS(new HttpProvider(rpcUrl))
 let uportClient
 
 const createuPortClient = () => {
   return deploy(rpcUrl)
     .then(res => {
-      console.log(res)
-      const config = Object.assign({  id: '5777', rpcUrl }, res)
+      const config = { network: { id: '5777', rpcUrl, registry: res.Registry, identityManager: res.IdentityManager } }
       uportClient = new UPortClient(config)
-      console.log(uportClient)
       uportClient.initKeys()
       return eth.coinbase()
     .then(address => {
       // Fund device key Transaction
       const fundTx = { to: uportClient.deviceKeys.address,
-                       value: 0.1 * 1.0e18,
-                       from: address }
+                       value: 0.5 * 1.0e18,
+                       from: address,
+                       data: '0x' }
       return eth.sendTransaction(fundTx)})
     .then( txHash => {
       console.log('Funded device key')
-      return mockClient.initializeIdentity()
+      return uportClient.initializeIdentity()
     }).then(res => {
       console.log('Identity Created')
       return uportClient
@@ -40,4 +39,4 @@ createuPortClient().then(client => {
   // return client.consume(uri)
 }).then(res => {
   // Handle Response
-})
+}).catch(console.log)
